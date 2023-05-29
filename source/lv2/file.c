c0d4d9aba6803ab1a95fef3399fc62cae0983aec
Emma <webmaster@invoxiplaygames.uk>
2023-05-29 23:56:07 +0100

    Fix loading gzip files in stage2 (#12)

--- source/lv2/file.c
+++ source/lv2/file.c
@@ -9,7 +9,6 @@ used for zlib support ...
 #include <string.h>
 #include <assert.h>
 #include <string.h>
-//#include <zlib.h>
 #include <xetypes.h>
 #include <elf/elf.h>
 #include <network/network.h>
@@ -26,9 +25,7 @@ used for zlib support ...
 #include "tftp/tftp.h"
 #include "../lv1/puff/puff.h"
 
-#define CHUNK 16384
-
-//int i;
+#define GZIP_HEADER_SIZE 10
 
 extern char dt_blob_start[];
 extern char dt_blob_end[];
@@ -47,59 +44,6 @@ struct filenames filelist[] = {
 	{" ", TYPE_ELF}
     //{NULL, NULL} //Dunno why this is here? :S
 };
-//Decompress a gzip file ...
-//int inflate_read(char *source,int len,char **dest,int * destsize, int gzip) {
-//	int ret;
-//	unsigned have;
-//	z_stream strm;
-//	unsigned char out[CHUNK];
-//	int totalsize = 0;
-//
-//	/* allocate inflate state */
-//	strm.zalloc = Z_NULL;
-//	strm.zfree = Z_NULL;
-//	strm.opaque = Z_NULL;
-//	strm.avail_in = 0;
-//	strm.next_in = Z_NULL;
-//	
-//	if(gzip)
-//		ret = inflateInit2(&strm, 16+MAX_WBITS);
-//	else
-//		ret = inflateInit(&strm);
-//		
-//	if (ret != Z_OK)
-//		return ret;
-//
-//	strm.avail_in = len;
-//	strm.next_in = (Bytef*)source;
-//
-//	/* run inflate() on input until output buffer not full */
-//	do {
-//		strm.avail_out = CHUNK;
-//		strm.next_out = (Bytef*)out;
-//		ret = inflate(&strm, Z_NO_FLUSH);
-//		assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-//		switch (ret) {
-//		case Z_NEED_DICT:
-//			ret = Z_DATA_ERROR;     /* and fall through */
-//		case Z_DATA_ERROR:
-//		case Z_MEM_ERROR:
-//			inflateEnd(&strm);
-//			return ret;
-//		}
-//		have = CHUNK - strm.avail_out;
-//		totalsize += have;
-//                if (totalsize > ELF_MAXSIZE)
-//                    return Z_BUF_ERROR;
-//		//*dest = (char*)realloc(*dest,totalsize);
-//		memcpy(*dest + totalsize - have,out,have);
-//		*destsize = totalsize;
-//	} while (strm.avail_out == 0);
-//
-//	/* clean up and return */
-//	(void)inflateEnd(&strm);
-//	return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
-//}
 
 void wait_and_cleanup_line()
 {
@@ -120,19 +64,19 @@ int launch_file(void * addr, unsigned len, int filetype){
             gzip_file = (unsigned char *)addr;
             if((gzip_file[0]==0x1F)&&(gzip_file[1]==0x8B)){
 				//found a gzip file
-                printf(" * Found a gzip file...\n");
+                printf(" * Found a gzip file, unpacking...\n");
                 char * dest = malloc(ELF_MAXSIZE);
-                long unsigned int destsize = 0;
-                //if(inflate_read((char*)addr, len, &dest, &destsize, 1) == 0){
-				if (puff((unsigned char*)dest, &destsize, addr, (long unsigned int*)&len)==0){
+                long unsigned int destsize = ELF_MAXSIZE;
+                int err = puff((unsigned char *)dest, &destsize, &gzip_file[GZIP_HEADER_SIZE], (long unsigned int *)&len);
+				if (err == 0) {
 					//relocate elf ...
                     memcpy(addr,dest,destsize);
-                    printf(" * Successfully unpacked...\n");
+                    printf(" * Successfully unpacked %li bytes...\n", destsize);
                     free(dest);
                     len=destsize;
                 }
                 else {
-					printf(" * Unpacking failed...\n");
+					printf(" * Unpacking failed with error %i...\n", err);
                     free(dest);
                     return -1;
                 }
